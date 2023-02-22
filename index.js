2529f319f59e50577c743df34be5fa62efa71681
sozankhalil <sozan.civil@gmail.com>
2023-02-22 12:10:03 +0300

    adding image

--- index.js
+++ index.js
@@ -1,73 +1,76 @@
-const main=document.querySelector('.main');
-const alive=document.querySelector('.alive');
-const h2=document.querySelector('h2');
+const main = document.querySelector('.main');
+const alive = document.querySelector('.alive');
+const h2 = document.querySelector('h2');
 
-const btn=document.querySelector('.btn');
+const btn = document.querySelector('.btn');
 
 
-async function myFunction(){
+
+async function myFunction(url) {
     try {
-        const response = await fetch('https://rickandmortyapi.com/api/character');
+        const response = await fetch(url);
 
         if (!response.ok) {
             throw new Error(`HTTP error: ${response.status}`);
         }
 
         const datas = await response.json();
-        
+
         for (const data of datas.results) {
-        
-            if (data.episode.length > 25 ) {
-                data.character='Main';
+
+            if (data.episode.length > 25) {
+                data.character = 'Main';
             } else {
                 data.character = 'side';
             }
-               
+
         }
-        for (let i = 0; i < datas.results.length; i++) {
+        // for (let i = 0; i < datas.results.length; i++) {
 
-                const div = document.createElement('div');
+        //         const div = document.createElement('div');
 
-                div.innerHTML = `<ul>
-                <li>Id: ${datas.results[i].id}</li>
-                <li>Name: ${datas.results[i].name}</li>
-                <li>Character: ${datas.results[i].character}</li>
-                <li>Status: ${datas.results[i].status}</li>
-                <li>Species: ${datas.results[i].species}</li>
-                <li>Gender: ${datas.results[i].gender}</li>
-                <li>Origin: ${datas.results[i].origin.name}</li>
-                </ul>`;
-                main.appendChild(div);
-            }
-    
-               for (const data of datas.results) {
-                if ( data.status==='Alive') {
+        //         div.innerHTML = `<ul>
+        //         <li>Id: ${datas.results[i].id}</li>
+        //         <li>Name: ${datas.results[i].name}</li>
+        //         <li>Character: ${datas.results[i].character}</li>
+        //         <li>Status: ${datas.results[i].status}</li>
+        //         <li>Species: ${datas.results[i].species}</li>
+        //         <li>Gender: ${datas.results[i].gender}</li>
+        //         <li>Origin: ${datas.results[i].origin.name}</li>
+        //         </ul>`;
+        //         main.appendChild(div);
+        //     }
 
-                    h2.innerText='Characters who are alive';
-                    const div1=document.createElement('div');
-                    div1.innerHTML =`<ul>
+        for (const data of datas.results) {
+            if (data.status === 'Alive') {
+
+                h2.innerText = 'Characters who are alive';
+                const div1 = document.createElement('div');
+                div1.innerHTML = `
+                    <img src="${data.image}" alt="">
+                    <ul>
                 <li>Name: ${data.name}</li>
                 <li>Character: ${data.character}</li>
                 <li>Species: ${data.species}</li>
                 <li>Gender: ${data.gender}</li>
                 <li>Origin: ${data.origin.name}</li>
                     </ul>`
-                   
-                    alive.appendChild(div1);
-                }
-               }
-                
-            }
 
-     catch (error) {
+                alive.appendChild(div1);
+            }
+        }
+
+    }
+
+    catch (error) {
         console.error(`Could not get products: ${error}`);
 
     }
- 
-    
-}
-myFunction();
 
+
+}
+
+myFunction('https://rickandmortyapi.com/api/character');
 
 
 
